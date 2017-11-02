b8d7b1808570c59ed3db12a710af7f533d60a604
Ange Near <handofbug@gmail.com>
2017-11-02 18:37:56 +0300

    fix

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
+/* jshint -W119, -W104*/
 module.exports = function (fileName = 'db.json', adapterName = 'FileSync', encrypt = false) {
     const low = require('lowdb'),
         FileSync = require('lowdb/adapters/FileSync');
-    switch (adapterName) {
-        case 'FileSync':
-            const adapter = new FileSync(fileName);
-            const db = low(adapter);
-            return db;
-            break;
-
+    switch (adapterName.toLowerCase()) {
+        case 'filesync':
+            const adapterFileSync = new FileSync(fileName);
+            return low(adapterFileSync);
+        case 'memory':
+            return low();
         default:
             return null;
-            break;
     }
-
 }
---
+++ test.js
@@ -0,0 +1,18 @@
+let db = require('./index')();
+
+db.defaults({
+        posts: [],
+        user: {}
+    })
+    .write();
+
+db.get('posts').push({
+        id: 2,
+        title: 'lowdb is awesome'
+    })
+    .write();
+let data = db.get('posts').find({
+    id: 2
+}).value();
+
+console.log(data);
